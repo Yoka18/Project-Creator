2943380750ae265590fec467dc92c4c2b03eaaa9
inmisin <omeroflu2@gmail.com>
2022-10-23 20:37:47 +0300

    Project Creator v1.1

--- Project Creator/Program.cs
+++ Project Creator/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 
+ProcessStartInfo startInfo = new ProcessStartInfo();
 
 Console.WriteLine("1. React.js");
 Console.WriteLine("2. Vue.js");
@@ -14,27 +15,39 @@ Console.WriteLine("Enter your project name?");
 string pname = Console.ReadLine();
 Console.WriteLine("Enter your project location");
 string loc = Console.ReadLine();
-string react = "npx create-react-app "+pname;
+string react = "npx create-react-app " + pname;
+string next = "npx create-next-app " + pname;
+string vue = "npm init vue@latest " + pname;
+
 
 switch (project)
 {
     case 1:
-        ProcessStartInfo startInfo = new ProcessStartInfo();
         startInfo.FileName = "CMD.exe";
         startInfo.Arguments = "/k" + react;
         startInfo.UseShellExecute = false;
         startInfo.WorkingDirectory = loc;
         startInfo.Verb = "runas";
         Process.Start(startInfo);
+        Process.Start("explorer.exe", loc);
         break;
     case 2:
-        Console.WriteLine("Vue");
+        startInfo.FileName = "CMD.exe";
+        startInfo.Arguments = "/k" + vue;
+        startInfo.UseShellExecute = false;
+        startInfo.WorkingDirectory = loc;
+        startInfo.Verb = "runas";
+        Process.Start(startInfo);
+        Process.Start("explorer.exe", loc);
         break;
     case 3:
-        Console.WriteLine("Next");
-        break;
-    case 4:
-        Console.WriteLine("test");
+        startInfo.FileName = "CMD.exe";
+        startInfo.Arguments = "/k" + next;
+        startInfo.UseShellExecute = false;
+        startInfo.WorkingDirectory = loc;
+        startInfo.Verb = "runas";
+        Process.Start(startInfo);
+        Process.Start("explorer.exe", loc);
         break;
     default:
         break;
